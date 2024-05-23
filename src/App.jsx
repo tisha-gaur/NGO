55e8fc143e96547dc36fec8154d69e2c28ab5945
ravinainn <nain.ravi12345@gmail.com>
2024-05-23 18:49:54 +0530

    initializing folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 const App = () => {
   return (
-    <div>
-      <h1>Family Health Education</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
---
+++ src/pages/about.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>About section</div>;
+};
+
+export default About;
