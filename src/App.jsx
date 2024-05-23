6680ac64f935f0ca8c11126eb6c68ecd621c4178
ravinainn <nain.ravi12345@gmail.com>
2024-05-23 19:17:37 +0530

    teaching tisha about react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
+import NavBar from "./components/navBar";
 
 const App = () => {
   return (
