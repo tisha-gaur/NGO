276c72242b334a9ebfdd2dfa901381cb5f30448f
ravinainn <nain.ravi12345@gmail.com>
2024-05-23 18:27:54 +0530

    changed app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <h1>Hello World!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
