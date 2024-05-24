7edf1263814b058d7c6451011c6326c8a48ac9e5
tisha-gaur <tisha98gaur@gmail.com>
2024-05-24 18:57:59 +0530

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/about";
-import NavBar from "./components/navBar";
+// import Home from "./pages/home";
+// import About from "./pages/about";
+// import NavBar from "./components/navBar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+      <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} /> */}
         </Routes>
       </BrowserRouter>
     </>
