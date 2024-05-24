d4363d037b36058143e0d8bd60ff704e8e805512
ravinainn <nain.ravi12345@gmail.com>
2024-05-24 21:34:25 +0530

    added heading to about us section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./pages/home";
-// import About from "./pages/about";
-// import NavBar from "./components/navBar";
+import Home from "./pages/home";
+import About from "./pages/about";
+
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <Header />
+        <Header />
         <Routes>
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Home />} /> */}
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,55 @@
-import React, { Component, useState } from 'react';
-import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
-import logo from '../assets/logoFHE.png';
+import React, { Component, useState } from "react";
+import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import logo from "../assets/logoFHE.png";
 
 const Header = () => {
-    let Links =[
-        {name:"HOME",link:"/"},
-        {name:"GALLERY",link:"/"},
-        {name:"ABOUT",link:"/"},
-        {name:"PROGRAMS",link:"/"},
-        {name:"VOLUTEER US",link:"/"},
-      ];
-      let [open, setOpen] =useState(false);
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "GALLERY", link: "/" },
+    { name: "ABOUT", link: "/about" },
+    { name: "PROGRAMS", link: "/" },
+    { name: "VOLUTEER US", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
 
-    return (
-        <div className='shadow-md w-full fixed top-0 left-0'>
-           <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-            {/* logo section */}
-            <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-                <img src={logo} className='ml-2 w-14 h-14 text-blue-600'/>
-            </div>
-            {/* Menu icon */}
-            <div onClick={()=>setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
-                {
-                    open ? <XMarkIcon/> : <Bars3BottomRightIcon />
-                }
-            </div>
-            {/* linke items */}
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
-                {
-                    Links.map((link) => (
-                    <li className='md:ml-8 md:my-0 my-7 font-semibold'>
-                        <a href={link.link} className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</a>
-                    </li>))
-                }
-                <button className='btn bg-blue-600 text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static'>DONATE</button>
-            </ul>
-            {/* button */}
-           </div>
+  return (
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        {/* logo section */}
+        <div className="font-bold text-2xl cursor-pointer flex items-center gap-1">
+          <img src={logo} className="ml-2 w-14 h-14 text-blue-600" />
         </div>
-    );
+        {/* Menu icon */}
+        <div
+          onClick={() => setOpen(!open)}
+          className="absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7"
+        >
+          {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
+        </div>
+        {/* linke items */}
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-12" : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li className="md:ml-8 md:my-0 my-7 font-semibold">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-blue-400 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+          <button className="btn bg-blue-600 text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static">
+            DONATE
+          </button>
+        </ul>
+        {/* button */}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
